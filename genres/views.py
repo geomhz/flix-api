e428e4ff0d957bc39be6d73c27d4a594d398b2fa
Geovanne Murata <geovanne1997@hotmail.com>
2024-04-22 16:57:12 -0300

    Config. expiração token bearer, refresh/verify token, permissão dinamica do crud de acordo com  GROUPS do DJANGO ADMIN

--- genres/views.py
+++ genres/views.py
@@ -2,13 +2,14 @@ from rest_framework import generics
 from genres.models import Genre
 from genres.serializers import GenreSerializer
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)    
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)    
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
