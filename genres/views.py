1e99c7a698882784e17a6235bb2530af34367897
Geovanne Murata <geovanne1997@hotmail.com>
2024-04-22 13:52:14 -0300

    JWT (Json Web Token) + Validação de token valido para ter acesso á API

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from genres.models import Genre
 from genres.serializers import GenreSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class GenreCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)    
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,15 @@
 from rest_framework import generics
 from movies.models import Movie
 from movies.serializers import MovieSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class MovieCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
